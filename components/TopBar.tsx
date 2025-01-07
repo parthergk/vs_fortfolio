3d6339cb33959cf6ac49745a3a798c94864b4cb3
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-07 20:25:06 +0530

    close-tab-logic

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,15 +1,24 @@
+'use client'
 import { X } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const TopBar = () => {
+    function handleClose (){
+        window.close();
+    }
   return (
     <div className=" h-12 px-3 border-b bg-customBlack flex items-center justify-between border-neutral-800">
+      <Link href='/'>
       <Image alt="img" src="/image/vsIcon.png" width={20} height={20} />
+      </Link>
       <div className=" w-60 sm:w-96 text-center border border-neutral-700 bg-neutral-800 rounded-md">
         <span className=" text-sm">Full Stack Developer</span>
       </div>
-      <X className=" w-5 h-5 " />
+      <div className=" p-1 cursor-pointer" onClick={handleClose}>
+        <X className=" w-5 h-5 " />
+      </div>
     </div>
   );
 };
